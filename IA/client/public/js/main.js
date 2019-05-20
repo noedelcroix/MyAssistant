f4ab6b59f031f07cfc4c3ddbd00ae24211735b05
noedelcroix <noe.delcroix@inracibe.onmicrosoft.com>
2019-05-20 16:20:04 +0200

    m

--- IA/client/public/js/main.js
+++ IA/client/public/js/main.js
@@ -1,5 +1,32 @@
-$(document).ready(()=>{
-    $("#submit").on("click", ()=>{
-        $("body").append("<div class=\"data>\">"+$("#request").val()+"</div>");
-    })
-})
+let sendMessage = (message, from)=>{
+    if(from == "request"){
+        $("body").append("<div class=\"flex request\"><div class=\"data request\">" + message + "</div></div>");
+    }else{
+        $("body").append("<div class=\"flex response\"><div class=\"data response\">" + message +"</div></div>");
+    }
+}
+let traiterRequest = () => {
+    let request = $("#request").val();
+    if (request === "") { return false; }
+    sendMessage(request, "request");
+    request = request.split(" ");
+    $("#request").val("");
+
+    if (request.indexOf("ouvrir") != -1) {
+        sendMessage("ouvrir", "response");
+    } else if (request.indexOf("dire") != -1) {
+        sendMessage("dire", "response");
+    }
+}
+
+$(document).ready(() => {
+    $("#submit").on("click", () => {
+        traiterRequest();
+    });
+
+    $(document).on("keydown", (event) => {
+        if(event.key === "Enter"){
+            traiterRequest();
+        }
+    });
+});
